3e87bea22888eac3ee1d049f1af0bcf5217b0108
SUPRIA <supriajaya02@gmail.com>
2025-06-28 02:27:22 +0700

    Delete firebase.js
    Signed-off-by: SUPRIA <supriajaya02@gmail.com>

--- firebase.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyCtpGW3wFjl4NINK9O3g4uP_N8Gc3T3sTY",
-  authDomain: "sosmedmini.firebaseapp.com",
-  projectId: "sosmedmini",
-  storageBucket: "sosmedmini.appspot.com",
-  messagingSenderId: "103701924545",
-  appId: "1:103701924545:web:53d4d4d3575e7ab167a240"
-};
